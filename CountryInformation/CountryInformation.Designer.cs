59974ab874904e0b39f2c7e5e4e6813732e929f8
Isaiah Lacey <laceyi2025@gmail.com>
2020-03-05 00:35:24 -0500

    Getting there

--- CountryInformation/CountryInformation.Designer.cs
+++ CountryInformation/CountryInformation.Designer.cs
@@ -75,6 +75,10 @@
          // 
          // txtSearchBar
          // 
+         this.txtSearchBar.AutoCompleteCustomSource.AddRange(new string[] {
+            "[countries]"});
+         this.txtSearchBar.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+         this.txtSearchBar.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
          this.txtSearchBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
          this.txtSearchBar.ForeColor = System.Drawing.Color.DarkBlue;
          this.txtSearchBar.Location = new System.Drawing.Point(1531, 892);
